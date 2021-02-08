643347dc300e06e49a9793189c4672b6fa289e7f
Nate <7nater.nelson@gmail.com>
2021-02-08 14:51:27 -0700

    commit tree afaaad191dc277e05bb28034ae057b183f04071a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color, temperament) VALUES ("Bob", 5,"M","Black", "Shy");
+INSERT INTO bears (name,age,sex,color, temperament) VALUES ("Lila", 4, "F","Black", "Shy");
+INSERT INTO bears (name,age,sex,color, temperament) VALUES ("Steve", 6, "M","White", "Lazy");
+INSERT INTO bears (name,age,sex,color, temperament) VALUES ("Wendy", 3, "F","White", "Agressive");
+INSERT INTO bears (name,age,sex,color, temperament) VALUES ("Iron", 8, "M","Black", "Agressive");
+INSERT INTO bears (name,age,sex,color, temperament) VALUES ("Spotty", 5, "F","Brown", "Shy");
+INSERT INTO bears (name,age,sex,color, temperament) VALUES ("Bob", 5, "M","Black", "Shy");
+INSERT INTO bears (name) VALUES (Null);
